95339d91722dde113a8d2a35e799f03abe945176
Riley Hemphill <rileyhemp@gmail.com>
2020-06-17 02:45:25 -0400

    Finished Array API

--- ECMAScript 6/Array API.js
+++ ECMAScript 6/Array API.js
@@ -59,22 +59,178 @@ describe("`Array.of` creates an array with the given arguments as elements", () 
 
 describe("`Array.prototype.fill` can fill up an array with one value", () => {
 	it("`fill(0)` will populate `0` into each array element", function () {
-		const arr = new Array(3).fill();
+		const arr = new Array(3).fill(0);
 		assert.deepEqual(arr, [0, 0, 0]);
 	});
 	it("fill only changes content, adds no new elements", function () {
-		const arr = [undefined].fill(0);
+		const arr = [].fill(0);
 		assert.deepEqual(arr, []);
 	});
 	it("second parameter to `fill()` is the position where to start filling", function () {
-		const fillPosition = 0;
+		const fillPosition = 2;
 		const arr = [1, 2, 3].fill(42, fillPosition);
 		assert.deepEqual(arr, [1, 2, 42]);
 	});
 	it("third parameter is the position where filling stops", function () {
 		const fillStartAt = 1;
-		const fillEndAt = 1;
+		const fillEndAt = 2;
 		const arr = [1, 2, 3].fill(42, fillStartAt, fillEndAt);
 		assert.deepEqual(arr, [1, 42, 3]);
+	});
+});
+
+// 32: array - `Array.prototype.find`
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe("`Array.prototype.find` makes finding items in arrays easier", () => {
+	it("takes a compare function", function () {
+		const found = [true].find((item) => item === true);
+		assert.equal(found, true);
+	});
+	it("returns the first value found", function () {
+		const found = [0, 2].find((item) => item > 1);
+		assert.equal(found, 2);
+	});
+	it("returns `undefined` when nothing was found", function () {
+		const found = [1, 2, 3].find((item) => item === 4);
+		assert.equal(found, void 0);
+	});
+	it("combined with destructuring complex compares become short", function () {
+		const bob = { name: "Bob" };
+		const alice = { name: "Alice" };
+		const found = [bob, alice].find(({ name }) => name === "Alice");
+		assert.equal(found, alice);
+	});
+});
+
+// 33: array - `Array.prototype.findIndex`
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe("`Array.prototype.findIndex` makes finding items in arrays easier", () => {
+	it("takes a compare function, returns the index where it returned true", function () {
+		const foundAt = [false, true].findIndex((item) => item === true);
+		assert.equal(foundAt, 1);
+	});
+	it("returns the first position it was found at", function () {
+		const foundAt = [0, 1, 1, 1].findIndex((item) => item === 1);
+		assert.equal(foundAt, 1);
+	});
+	it("returns `-1` when nothing was found", function () {
+		const foundAt = [1, 2, 3].findIndex((item) => item > 3);
+		assert.equal(foundAt, -1);
+	});
+	it("the findIndex callback gets the item, index and array as arguments", function () {
+		const three = 3;
+		const containsThree = (arr) => arr.indexOf(three) > -1;
+
+		function theSecondThree(element, index, arr) {
+			const preceedingItems = arr.slice(0, index);
+			return containsThree(preceedingItems);
+		}
+
+		const foundAt = [1, 1, 2, 3, 3, 3].findIndex(theSecondThree);
+		assert.equal(foundAt, 4);
+	});
+	it("combined with destructuring complex compares become short", function () {
+		const bob = { name: "Bob" };
+		const alice = { name: "Alice" };
+		const foundAt = [bob, alice].findIndex(({ name: { length } }) => length > 3);
+		assert.equal(foundAt, 1);
+	});
+});
+
+// 41: array - entries
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe("`[].entries()` returns an iterator object with all entries", function () {
+	it("returns key+value for each element", function () {
+		const arr = ["a", "b", "c"];
+		const entriesAsArray = Array.from(arr.entries());
+		assert.deepEqual(entriesAsArray, [
+			[0, "a"],
+			[1, "b"],
+			[2, "c"],
+		]);
+	});
+	it("empty elements contain the value `undefined`", function () {
+		const arr = ["one"];
+		arr[2] = "three";
+		const secondValue = Array.from(arr.entries())[1];
+		assert.deepEqual(secondValue, [1, void 0]);
+	});
+	describe("returns an iterable", function () {
+		it("has `next()` to iterate", function () {
+			const arr = ["one"];
+			const value = Array.from(arr.entries())[0];
+			assert.deepEqual(value, [0, "one"]);
+		});
+	});
+});
+
+// 42: array - `Array.prototype.keys`
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe("`Array.prototype.keys` returns an iterator for all keys in the array", () => {
+	it("`keys()` returns an iterator", function () {
+		const arr = ["a"];
+		const iterator = arr.keys();
+		assert.deepEqual(iterator.next(), { value: 0, done: false });
+		assert.deepEqual(iterator.next(), { value: void 0, done: true });
+	});
+	it("gets all keys", function () {
+		const arr = ["a", "b", "c"];
+		const keys = Array.from(arr.keys());
+		assert.deepEqual(keys, [0, 1, 2]);
+	});
+	it("empty array contains no keys", function () {
+		const arr = [];
+		const keys = [...arr.keys()];
+		assert.equal(keys.length, 0);
+	});
+	it("a sparse array without real values has keys though", function () {
+		const arr = [, ,];
+		const keys = [...arr.keys()];
+		assert.deepEqual(keys, [0, 1]);
+	});
+	it("also includes holes in sparse arrays", function () {
+		const arr = ["a", , "c"];
+		const keys = Array.from(arr.keys());
+		assert.deepEqual(keys, [0, 1, 2]);
+	});
+});
+
+// 43: array - `Array.prototype.values`
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe("`Array.prototype.values` returns an iterator for all values in the array", () => {
+	it("`values()` returns an iterator", function () {
+		const arr = [];
+		const iterator = arr.values();
+		assert.deepEqual(iterator.next(), { value: void 0, done: true });
+	});
+	it("use `iterator.next()` to drop first value", function () {
+		const arr = ["keys", "values", "entries"];
+		const iterator = arr.values();
+		iterator.next();
+		assert.deepEqual([...iterator], ["values", "entries"]);
+	});
+	it("empty array contains no values", function () {
+		const arr = [...[...[...[]]]];
+		const values = [...arr.values()];
+		assert.equal(values.length, 0);
+	});
+	it("a sparse array without real values has values though", function () {
+		const arr = [, ,];
+		const keys = [...arr.values()];
+		assert.deepEqual(keys, [void 0, void 0]);
+	});
+	it("also includes holes in sparse arrays", function () {
+		const arr = ["a", , "c"];
+		assert.deepEqual([...arr.values()], ["a", void 0, "c"]);
 	});
 });
